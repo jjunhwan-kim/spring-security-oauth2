0c24e9c2b70f6ccb2ba8b7b206c340b5e9556901
JunHwan Kim <nefertiri972@gmail.com>
2022-07-28 23:26:26 +0900

    Add OAuth config

---
+++ src/main/java/com/example/security/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.example.security.config.auth.dto;
+
+import com.example.security.domain.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
---
+++ src/main/java/com/example/security/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.security.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, Role role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public User update(String name) {
+        this.name = name;
+
+        return this;
+    }
+}
